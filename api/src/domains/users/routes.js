634823b852263c5950753aa56e04b7956d4db88b
trevorregister <trevor.register@gmail.com>
2025-02-03 09:16:08 -0500

    save state

--- api/src/domains/users/routes.js
+++ api/src/domains/users/routes.js
@@ -1,12 +1,8 @@
 import express from 'express'
 import UserController from './controller.js'
-import UserRepository from './repository.js'
-import UserModel from './data-access/model.js'
 import userUseCases from './use-cases/index.js'
 
 export default function userRoutes(){
-    const model = new UserModel()
-    const repository = new UserRepository(model)
     const controller = new UserController(userUseCases)
     const router = express.Router()
     router.post('/', controller.create)
