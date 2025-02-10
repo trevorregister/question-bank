9c100853b8343074bfceba6b235dfbaa96933a61
trevorregister <trevor.register@gmail.com>
2025-02-10 11:02:38 -0500

    feat: question int tests

---
+++ api/src/__tests__/integration/questions/CreateQuestion.js
@@ -0,0 +1,49 @@
+const builder = require("../../../db-seed/builder.js")
+const generateId = require("../../../domains/utils/generateId.js")
+const request = require('../setup.js')
+const { faker } = builder
+
+
+describe('Create Question', () => {
+
+    it('given valid inputs, returns new question and 201', async () => {
+        const questionOwner = generateId()
+        const questionProps = {
+            prompt: faker.lorem.sentence(10),
+            pointValue: faker.number.int({min: 10, max: 50}),
+            type: 'numerical',
+            owner: questionOwner
+        }
+        const res = await request.questions.post('/', questionProps)
+        expect(res.status).toBe(201)
+        const { id, prompt, variables, conditions, pointValue, owner, type } = res.body
+        expect({
+            id,
+            prompt,
+            variables,
+            conditions,
+            pointValue,
+            owner,
+            type
+        }).toEqual({
+            id: id,
+            prompt: prompt,
+            variables: variables,
+            conditions: conditions,
+            pointValue: pointValue,
+            owner: owner,
+            type: type
+        })
+    })
+
+    it('given invalid inputs, returns 422', async () => {
+        const questionProps = {
+            prompt: faker.lorem.sentence(10),
+            pointValue: faker.number.int({min: 10, max: 50}),
+            type: 'asdf',
+            owner: '123'
+        }
+        const res = await request.questions.post('/', questionProps)
+        expect(res.status).toBe(422)
+    })
+})
