6fbd2ed17974714050dc22a0985477f861e7baf0
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2025-02-10 19:08:13 -0500

    test

--- api/src/__tests__/integration/questions/UpdateQuestion.js
+++ api/src/__tests__/integration/questions/UpdateQuestion.js
@@ -1,4 +1,5 @@
 const builder = require("../../../db-seed/builder.js")
+const generateId = require("../../../domains/utils/generateId.js")
 const request = require('../setup.js')
 const { faker } = builder
 
@@ -21,5 +22,45 @@ describe('Update question', () => {
             prompt: prompt,
             pointValue: pointValue
         })
+    })
+
+    it('returns question with updated variables/conditions and 201', async () => {
+        const variables = [
+            {
+                id: generateId(),
+                min: faker.number.int({min: 1, max: 10}),
+                max: faker.number.int({min: 11, max: 20}),
+                step: faker.number.int()
+            },
+        ]
+        const conditions = [
+            {
+                id: generateId(),
+                isCorrect: true,
+                feedback: faker.lorem.sentence(10),
+                expression: faker.lorem.sentence(10)
+            }
+        ]
+        const question = await builder.question({ variables: variables, conditions: conditions})
+        const variableUpdate = {
+            id: variables[0].id,
+            min: faker.number.int({min: 1, max: 10}),
+            max: faker.number.int({min: 11, max: 20}),
+            step: faker.number.int()
+        }
+        const conditionUpdate = {
+            id: conditions[0].id,
+            isCorrect: false,
+            feedback: faker.lorem.sentence(10),
+            expression: faker.lorem.sentence(10)
+        }
+        const payload = {
+            variables: [variableUpdate],
+            //conditions: [conditionUpdate]
+        }
+        const res = await request.questions.patch(`/${question._id}/`, payload)
+
+        expect(res.status).toBe(201)
+        console.log(res.body)
     })
 })
