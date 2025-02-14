dd1129107f225b20949f0ace7bf2f6afce6b7867
trevorregister <trevor.register@gmail.com>
2025-02-14 10:39:47 -0500

    feat: personal bank on account creation

--- api/src/domains/users/use-cases/CreateUser.js
+++ api/src/domains/users/use-cases/CreateUser.js
@@ -1,6 +1,10 @@
 const UseCase = require('../../../core/usecase')
 const User = require('../entities.js')
+const Bank = require('../../banks/entities.js')
+const BankModel = require('../../banks/data-access/model.js')
 const { HttpError } = require('../../../core/errors.js')
+const mongoose = require('mongoose')
+const { USER_ROLES } = require('../../../core/enums.js')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
@@ -10,21 +14,36 @@ module.exports = class CreateUserUseCase extends UseCase {
     }
 
     async execute ({email, firstName, lastName, role, password}) {
+        const session = await mongoose.startSession()
+        session.startTransaction()
+
         const existingUser = await this.repository.findByEmail(email)
-        
+            
         if(existingUser){
             throw new HttpError(422, `user with ${email} already exists`)
-        } else {
-        const salt = await bcrypt.genSalt(10)
-        const hash = await bcrypt.hash(password, salt)
-        const userProps = User.toDb({email, firstName, lastName, role, hash})
-        const user = await this.repository.create(new User(userProps))
-        if(user){
+        }
+
+        try {
+            const salt = await bcrypt.genSalt(10)
+            const hash = await bcrypt.hash(password, salt)
+            const userProps = User.toDb({email, firstName, lastName, role, hash})
+            const user = await this.repository.create(new User(userProps))
+    
+            if(role === USER_ROLES.Teacher){
+                const bankProps = Bank.toDb({ owner: user._id.toHexString(), name: 'Personal' }) 
+                const bank = await BankModel.create(new Bank(bankProps))
+            }
+
+                
+            await session.commitTransaction()
+            session.endSession()
+
             const token = jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET)
             return {token: token }
-        }else {
-            throw new HttpError(500, 'user not created')
-        }
+
+        } catch (err) {
+            await session.abortTransaction()
+            throw new HttpError(500, 'session commit error with user creation')
         }
     }
 }
