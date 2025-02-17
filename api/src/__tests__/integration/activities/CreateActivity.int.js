80d9cad0ea6e07984e69087e0206413b59ef2b41
trevorregister <trevor.register@gmail.com>
2025-02-17 14:25:43 -0500

    feat: 422 test for activity

--- api/src/__tests__/integration/activities/CreateActivity.int.js
+++ api/src/__tests__/integration/activities/CreateActivity.int.js
@@ -6,10 +6,28 @@ describe("Create Activity", () => {
   it("given valid inputs, returns new activity and 201", async () => {
     const user = await builder.user.teacher()
     const token = builder.token(user)
-    const props = { name: faker.lorem.sentence(5), owner: user._id }
+    const props = { name: faker.lorem.sentence(5) }
     const res = await request.activities.post("/", props, token)
 
     expect(res.status).toBe(201)
     expect(res.body.owner).toBe(user._id.toHexString())
+  })
+
+  it("given invalid props, returns 422", async () => {
+    const user = await builder.user.teacher()
+    const token = builder.token(user)
+    const props = { name: 123 }
+    const res = await request.activities.post("/", props, token)
+
+    expect(res.status).toBe(422)
+  })
+
+  it("given request from student, returns 403", async () => {
+    const user = await builder.user.student()
+    const token = builder.token(user)
+    const props = { name: faker.lorem.sentence(5) }
+    const res = await request.activities.post("/", props, token)
+
+    expect(res.status).toBe(403)
   })
 })
