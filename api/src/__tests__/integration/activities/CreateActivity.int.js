bffe23ddbbefb2849ed357b0a5abb752c7bfc4be
trevorregister <trevor.register@gmail.com>
2025-02-19 15:18:39 -0500

    fix: update test for new db model

--- api/src/__tests__/integration/activities/CreateActivity.int.js
+++ api/src/__tests__/integration/activities/CreateActivity.int.js
@@ -6,27 +6,73 @@ describe("Create Activity", () => {
   it("given valid inputs, returns new activity and 201", async () => {
     const user = await builder.user.teacher()
     const token = builder.token(user)
-    const sectionsProp = [builder.activity.section()]
+    const section = {
+      id: builder.randomId(),
+      name: faker.lorem.sentence(),
+      summary: faker.lorem.sentence(),
+      sectionIndex: 0,
+      questions: [{
+        parent: builder.randomId(),
+        type: "numerical",
+        prompt: faker.lorem.sentence(),
+        pointValue: faker.number.int({min: 1, max: 100}),
+        variables: [{
+          id: builder.randomId(),
+          type: 'random',
+          min: faker.number.int({min: 1, max: 10}),
+          max: faker.number.int({min: 11, max: 20}),
+          step: faker.number.int()
+        }],
+        conditions: [{
+          id: builder.randomId(),
+          expression: faker.lorem.sentence(),
+          isCorrect: faker.number.int()%2 === 0? true: false,
+          feedback: faker.lorem.sentence()
+
+        }]
+      }]
+    }
     const tagsProp = [faker.lorem.word(5)]
-    const props = { name: faker.lorem.sentence(5), sections: sectionsProp, tags: tagsProp }
+    const props = { name: faker.lorem.sentence(5), sections: [section], tags: tagsProp }
     const res = await request.activities.post("/", props, token)
-    const { id, owner, sections, isArchived, tags, questionCount, name } = res.body
-    
+    const { id, owner, sections, isArchived, tags, name } = res.body
+
     expect(res.status).toBe(201)
-    expect(owner).toBe(user._id.toHexString())
-    expect(id).toBeTruthy()
     expect({
+      owner,
       isArchived,
-      questionCount,
       name
     }).toEqual({
+      owner: user._id.toHexString(),
       isArchived: false,
-      questionCount: 0,
       name: props.name
     })
-    expect(sections[0].questions.length).toBe(sectionsProp[0].questions.length)
-    tags.forEach(t => {
-      expect(tagsProp.some(tag => t)).toBe(true)
+    expect(id).toBeTruthy()
+    expect(tags[0]).toBe(tagsProp[0])
+    expect(sections[0]).toEqual({
+      id: section.id.toHexString(),
+      name: section.name,
+      summary: section.summary,
+      sectionIndex: section.sectionIndex,
+      questions: [{
+        parent: section.questions[0].parent.toHexString(),
+        type: section.questions[0].type,
+        prompt: section.questions[0].prompt,
+        pointValue: section.questions[0].pointValue,
+        variables: [{
+          id: section.questions[0].variables[0].id.toHexString(),
+          type: section.questions[0].variables[0].type,
+          min: section.questions[0].variables[0].min,
+          max: section.questions[0].variables[0].max,
+          step: section.questions[0].variables[0].step
+        }],
+        conditions: [{
+          id: section.questions[0].conditions[0].id.toHexString(),
+          expression: section.questions[0].conditions[0].expression,
+          isCorrect: section.questions[0].conditions[0].isCorrect,
+          feedback: section.questions[0].conditions[0].feedback
+        }]
+      }]
     })
   })
 
