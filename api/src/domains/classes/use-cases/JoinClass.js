09d7acdf2db1ef8d7dbaa636932017e2003c7526
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2025-02-20 19:09:36 -0500

    feat: join class test

--- api/src/domains/classes/use-cases/JoinClass.js
+++ api/src/domains/classes/use-cases/JoinClass.js
@@ -2,6 +2,7 @@ const UseCase = require("../../../core/usecase")
 const { NotFoundError, HttpError } = require("../../../core/errors.js")
 const UserModel = require('../../users/data-access/model.js')
 const { RosteredStudent } = require('../entities.js')
+const { USER_ROLES } = require('../../../core/enums.js')
 
 module.exports = class JoinClassUseCase extends UseCase {
   constructor(repository) {
@@ -13,9 +14,12 @@ module.exports = class JoinClassUseCase extends UseCase {
     if(joinCode.length !== 8 || invalidJoinCodeCharacters.test(joinCode)){
       throw new HttpError(400, `${joinCode} invalid code. Join codes are 8 characters long with only letters and numbers`)
     }
-    const student = UserModel.findById(userId)
-    if(!student) {
+    const user = await UserModel.findById(userId)
+    if(!user) {
       throw new NotFoundError(`user ${owner}`)
+    }
+    if(user.role !== USER_ROLES.Student){
+      throw new HttpError(400, `user ${userId} not a student`)
     }
     const rosteredStudentProps = RosteredStudent.toDb(userId)
     const props = {joinCode: joinCode, studentToRoster: new RosteredStudent(rosteredStudentProps)}
