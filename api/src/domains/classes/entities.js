2cbd4e093d24c9424e7f739c4d7ce7960e2cc654
trevorregister <trevor.register@gmail.com>
2025-02-21 11:10:25 -0500

    ref: use toWeb for roster and dropped students

--- api/src/domains/classes/entities.js
+++ api/src/domains/classes/entities.js
@@ -59,8 +59,8 @@ class Class extends Entity {
       id: data._id,
       name: data.name,
       owner: data.owner,
-      roster: data.roster,
-      droppedStudents: data.droppedStudents,
+      roster: data.roster.map(s => RosteredStudent.toWeb(s)),
+      droppedStudents: data.droppedStudents.map(s => DroppedStudent.toWeb(s)),
       joinCode: data.joinCode
     }
   }
