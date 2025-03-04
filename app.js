41648902ceb8aaa53adb917b90c2a58eabd2f6c0
trevorregister <trevor.register@gmail.com>
2025-03-04 13:37:16 -0500

    feat: set cors options

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const dotenv = require("dotenv").config();
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const Routes = require("./routes");
-const errorHandler = require("./src/middleware/errorHandler");
-const subscribers = require('./src/events/subscribers/index')
+const express = require("express")
+const morgan = require("morgan")
+const helmet = require("helmet")
+const dotenv = require("dotenv").config()
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+const Routes = require("./routes")
+const errorHandler = require("./src/middleware/errorHandler")
+const subscribers = require("./src/events/subscribers/index")
 
-const app = express();
+const app = express()
 const corsOptions = {
-  //origin: process.env.WEB_HOST,
+  origin: process.env.WEB_HOST,
   credentials: true,
-};
+}
 
-app.use(helmet());
-app.use(cors(corsOptions));
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(morgan("dev"));
-app.use("/api", Routes());
-app.use(errorHandler);
+app.use(helmet())
+app.use(cors(corsOptions))
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan("dev"))
+app.use("/api", Routes())
+app.use(errorHandler)
 
-module.exports = app;
+module.exports = app
