ffedab58ce51af23dc9cd10742768e36f247f108
trevorregister <trevor.register@gmail.com>
2025-03-13 16:11:32 -0400

    fix: jwt secret

--- src/db-seed/builder.js
+++ src/db-seed/builder.js
@@ -304,7 +304,7 @@ class Builder {
     return generateId()
   }
   token(user) {
-    return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET)
+    return jwt.sign({ id: user._id, role: user.role }, "jwt_secret")
   }
   async seed() {
     return {
