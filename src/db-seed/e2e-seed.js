c0e6920b3c8f4f1f0a63f3bb20bc0d8fd810e803
trevorregister <trevor.register@gmail.com>
2025-03-20 21:10:03 -0400

    feat: activities e2e seed

--- src/db-seed/e2e-seed.js
+++ src/db-seed/e2e-seed.js
@@ -5,14 +5,17 @@ const ActivityModel = require("../domains/activities/data-access/model")
 const ClassModel = require("../domains/classes/data-access/model")
 const AssignmentModel = require("../domains/assignments/data-access/model")
 const AssignmentResponseModel = require("../domains/responses/data-access/model")
+const ActivitiesModel = require("../domains/activities/data-access/model")
 
 module.exports = async (seed) => {
   return Promise.all([
     await UserModel.deleteMany({}),
     await BankModel.deleteMany({}),
     await QuestionModel.deleteMany({}),
+    await ActivitiesModel.deleteMany({}),
     await UserModel.insertMany(seed.users ?? []),
     await QuestionModel.insertMany(seed.questions ?? []),
     await BankModel.insertMany(seed.banks ?? []),
+    await ActivitiesModel.insertMany(seed.activities ?? []),
   ])
 }
